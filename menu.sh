d70188fa3d204f80260d9ddc09090541dd99bb22
lautsoria <ltsoria@fi.uba.ar>
2025-08-27 10:17:01 -0300

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,87 +1,91 @@
 #!/bin/bash
-if [[ -n "${FILENAME+x}" ]]; then
+
+if [[ -z $FILENAME ]]; then
     echo "debes definir la variable de ambiente FILENAME"
     exit
 fi
-#si la variable no  esta definida o vacia
+# si la variable no esta definida o vacia
 
+directorio="/home/EPNro1"
+entrada="$directorio/entrada"
+file_name="$directorio/salida/$FILENAME"
+procesado="$directorio/procesado"
 
 a=0
-
-while [ "$a" -ne 6 ]
+while [[ "$a" -ne 6 ]]
 do
-    echo -e "\n\t------------------------------"
-    echo -e "\t------------------------------"
-    echo -e "Seleccionar una de las siguientes opciones:\n\t1 - Crear Entorno\n\t2 - Correr Proceso\n\t3 - Alumnos\n\t4 - Top 10\n\t5 - Buscar por padron\n\t6 - Salir"
-    echo -e "\n\t------------------------------"
-    echo -e "\t------------------------------\n"
+    echo -e "Seleccionar una de las siguientes opciones:\n\t1 - Crear Entorno\n\t2 - Correr Proceso\n\t3 - Alumnos\n\t4 - Top 10\n\t5 - Buscar por padron\n\t6 - Salir\n\td - borrar entorno ;c"
     read a
     case $a in
-        1) 
-            echo "seleccionaste crear entorno"
-             cd /home
+        1)
+            echo "Seleccionaste crear entorno"
+            cd /home
 
-    mkdir EPNro1 EPNro1/entrada EPNro1/salida EPNro1/procesado
-    touch EPNro1/salida/${FILENAME}
+            mkdir EPNro1 EPNro1/entrada EPNro1/salida EPNro1/procesado
+            touch EPNro1/salida/${FILENAME}
 
+            cat << EOF > /home/EPNro1/consolidar.sh
+#!/bin/bash
 
-    cat << EOF > /home/EPNro1/consolidar.sh
-        #!/bin/bash
+directorio="/home/EPNro1"
+entrada="$directorio/entrada"
+file_name="$directorio/salida/${FILENAME}"
+procesado="$directorio/procesado"
 
-        directorio="/home/EPNro1"
-        entrada="${directorio}/entrada"
-        file_name="${directorio}/salida/${FILENAME}"
-        procesado="${directorio}/procesado"
-
-        while true; do
-        find ${entrada} -type f -exec cat {} >> ${file_name} \; -exec echo >> ${file_name} \; -exec mv {} ${procesado}/ \;
-        done
+while true; do
+    find ${entrada} -type f -exec cat {} >> ${file_name} \; -exec echo >> ${file_name} \; -exec mv {} ${procesado}/ \;
+done
 EOF
-
-    echo -e "Entorno creado correctamente"
-    echo -e "\n------------\n"
-    ;;
-
-        2) 
-            
-            bash /home/EPNro1/consolidar.sh &
-            echo -e "\n------------\n"
+            echo -e "Entorno creado correctamente"
+            echo -e "\n-----------\n"
             ;;
+        2)  
+            process=$(pgrep -f "consolidar.sh")
 
-
-        3)
-            if [ -f "/home/lauta/Escritorio/so/salida/${FILENAME}" ]; then
-                cat "/home/lauta/Escritorio/so/salida/${FILENAME}" | sort -k1 -n
-            fi  
-            echo -e "\n------------\n"
-            ;;
-
-        4)
-            if [ -f "/home/lauta/Escritorio/so/salida/${FILENAME}" ]; then
-                cat "/home/lauta/Escritorio/so/salida/${FILENAME}" | sort -k5 -rn
+            if [[ -z $process ]]; then
+                bash /home/EPNro1/consolidar.sh &
+                echo -e "\n-----------\n"
             fi
-            echo -e "\n------------\n"
             ;;
-
+        3)
+            if [ -f "$file_name" ]; then
+                cat ""$file_name"" | sort -k1 -n
+            fi
+            echo -e "\n-----------\n"
+            ;;
+        4)
+            if [ -f "$file_name" ]; then
+                cat "$file_name" | sort -t';' -k4 -rn
+            fi
+            echo -e "\n-----------\n"
+            ;;
         5)
             echo ingrese numero de padron
 
             read x
+            #grep "^$x;" "$file_name"|echo "$1 $2 $3 $4"
 
-            while read -r c1 c2 c3 c4 c5; do
+            while IFS=";" read -r c1 c2 c3 c4 ; do
                 if [[ $c1 -eq $x ]]; then
-                    echo "$c1 $c2 $c3 $c4 $c5"
+                    echo "$c1 $c2 $c3 $c4 "
                 fi
-            done < "/home/lauta/Escritorio/so/salida/${FILENAME}"
-            echo -e "\n------------\n"
-            # simplicar alumnos .txt
-
-            # ver implementacion con grep
+            done < "$file_name"
+            echo -e "\n-----------\n"
             ;;
-        6) echo "saliendo"
-        echo -e "\n------------\n"
-        ;;
+            # ver implementacion con grep
+        6)
+            
+            pkill -f "consolidar.sh"
+            echo "saliendo"
+            echo -e "\n-----------\n"
+            ;;
+        d) 
 
+            sudo rm -r EPNro1
+            pkill -f "consolidar.sh"
+            echo "procesos y directorios borrados con exito ;c"
+            a=6
+            echo -e "\n-----------\n"
+            ;;
     esac
 done
-    
